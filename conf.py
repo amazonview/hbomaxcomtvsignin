202a59fd81e5f6e1ddafc801c32342380d78640b
amazonview <chetijasimran@gmail.com>
2025-06-30 13:05:54 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'Hbomax Tvsignin'
 
 
 # The full version, including alpha/beta/rc tags
